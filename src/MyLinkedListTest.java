39852551db7debf2345b9094bb47db36920d06c3
pratap1804 <pratap.1804@gmail.com>
2021-07-02 23:49:39 +0530

    Added: 30 and 56 to 70

---
+++ src/MyLinkedListTest.java
@@ -0,0 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+class MyLinkedListTest {
+
+	@Test
+	public void givem3NumbersWhenLinkedListShouldBeAddedToTop() {
+		 MyNode <Integer> myFirstNode = new MyNode<>(70);
+	       MyNode <Integer> mySecondNode = new MyNode<>(30);
+	       MyNode <Integer> myThirdNode = new MyNode<>(56);
+
+	       MyLinkedList myLinkedList = new MyLinkedList();
+	       myLinkedList.add(myFirstNode);
+	       myLinkedList.add(mySecondNode);
+	       myLinkedList.add(myThirdNode);
+	       myLinkedList.printMyNode();
+	
+	
+	       boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode)
+	    		   			&& myLinkedList.tail.equals(myFirstNode);
+	       Assert.assertTrue(result);
+	}
+}
--- src/MyNodeTest.java
+++ src/MyNodeTest.java
@@ -1,16 +1,17 @@
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Assert;
 
 class MyNodeTest {
 	@Test
 	public void givem3NumbersWhenLinkedListPassedLinkedListTest() {
-		MyNode<Integer> myFirstNode = new MyNode<>(56);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
-		myFirstNode.setNext(mySecondNode);
-		mySecondNode.setNext(myThirdNode);
-		boolean result = myFirstNode.getNext().equals(mySecondNode) &&
-                mySecondNode.getNext().equals(myThirdNode);
+	    MyNode <Integer> myFirstNode =  new MyNode<>(56);
+        MyNode <Integer> mySecondNode = new MyNode<>(30);
+        MyNode <Integer> myThirdNode =  new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) 
+				&& mySecondNode.getNext().equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
 }
