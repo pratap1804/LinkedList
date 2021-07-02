cfdb4f66b485a175dea7a68ffe2f0d676259ac99
pratap1804 <pratap.1804@gmail.com>
2021-07-02 20:48:14 +0530

    Created a linked list of 56, 30, 70

--- src/MyNode.java
+++ src/MyNode.java
@@ -1,7 +1,23 @@
 
-public class MyNode {
+public class MyNode<K> {
 
 	    
+	private K key;
+	private MyNode next;
+	
+	public MyNode(K key) {
+		this.key = key;
+		this.next = null;;
+	}
+
+	public MyNode getNext() {
+		return next;
+	}
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome To Linked List");
 
